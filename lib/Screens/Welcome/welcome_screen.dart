703327a2d8ab546b7a9116e48c37dfb780586f31
RBiswa787 <biswa7102raydeep@gmail.com>
2021-01-26 19:03:26 +0530

    Project Setup
    Added assets folder with icons and images subfolders. Created Screens folder under lib with Welcome subfolder and welcome_screen.dart file. Made constants.dart file under lib folder. Cleaned existing default code

---
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kprimarydark = Color(0xFF6F35A5);
+const kprimarylight = Color(0xFFF1E6FF);
